219b2dd95a7c7ed9567e3fece477a6268627125e
Muhammad Awais <90213892+F190922@users.noreply.github.com>
2022-12-14 16:49:32 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/common/Header/Header'
+import {BrowserRouter as Router} from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <>
+      
+      <Router>
+      <Header/>
+        
+    </Router>
+    </>
+  )
+}
+
+export default App
