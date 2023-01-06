157de445f10c11816f99936759be8b31a3825c39
Your Name <you@example.com>
2023-01-06 16:44:54 +0500

    some changes

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import Header from './components/common/Header/Header'
-import {BrowserRouter as Router} from "react-router-dom";
-
+import React from "react"
+import Header from "./components/common/header/Header"
+import "./App.css"
+import Homepages from "./components/home/Homepages"
+import Footer from "./components/common/footer/Footer"
+import SinglePage from "./components/singlePage/SinglePage"
+import Term from "./components/common/Term and Conditions/Term"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 const App = () => {
   return (
     <>
-      
       <Router>
-      <Header/>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepages} />
+          
+          <Route path='/singlepage/:id' exact component={SinglePage} />
+          <Route path="/Term" component={Term} />
+          <Footer/>
+
+        </Switch>       
         
-    </Router>
+        
+        
+      </Router>
     </>
   )
 }
